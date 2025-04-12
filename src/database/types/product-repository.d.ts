60f6a34c68f14ead104fdfd09555b04ef7761112
Jean Crus <jean.crus@outlook.com>
2025-04-11 22:23:34 -0300

    Adiciona definições de tipos para repositórios de pedidos, produtos, mesas e sessões de mesas, melhorando a estrutura do código e a tipagem no TypeScript.

---
+++ src/database/types/product-repository.d.ts
@@ -0,0 +1,7 @@
+type TableRepository = {
+  id: number;
+  table_number: number;
+  is_available: boolean;
+  created_at: string;
+  updated_at: string;
+};
---
+++ src/database/types/tables-sessions-repository.d.ts
@@ -0,0 +1,6 @@
+type TableSessionRepository = {
+  id: number;
+  table_id: number;
+  opened_at: string;
+  closed_at: string;
+};
